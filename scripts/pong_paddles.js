85813f71d079f71ad9ddcb4d40fcbc09c058aa34
Erin Leck <erinleck@gmail.com>
2017-12-19 11:23:10 -0700

    render pong objects

--- scripts/pong_paddles.js
+++ scripts/pong_paddles.js
@@ -6,35 +6,34 @@ function Paddle(x, y, width, height) {
   canvas_context.fillRect(x, y, width, height);
 }
 
-function Computer() {
-  new Paddle(0,175,20,150)
+function Player() {
+    new Paddle(780,175,20,150)
 }
 
-function Player() {
-  new Paddle(780,175,20,150)
+function Computer() {
+    new Paddle(0,175,20,150)
 }
 
 function Ball() {
-  canvas_context.fillStyle="#FFFFFF";
-  var ball_width = 10;
-  var ball_height = 10;
-  var x = 400;
-  var y = 250;
-  var cornerRadius = 5;
+  var x = canvas.width / 2;
+  var y = canvas.height / 2;
+  var radius = 5;
+  var startAngle = 0 * Math.PI;
+  var endAngle = 2 * Math.PI;
 
   canvas_context.beginPath();
-  canvas_context.moveTo(x,y);
-  canvas_context.lineTo(x + ball_width - cornerRadius, y);
-  canvas_context.arcTo(x + ball_width, y, x + ball_width, y + cornerRadius, cornerRadius);
-  canvas_context.lineTo(x + ball_width, y + ball_height);
-  canvas_context.arcTo(x - ball_width, y, x + ball_width, y + cornerRadius, cornerRadius);
-  canvas_context.lineWidth = 5;
-  canvas_context.strokeStyle="#FF0000";
+  canvas_context.arc(x, y, radius, startAngle, endAngle);
+  canvas_context.lineWidth = 10;
+  canvas_context.strokeStyle = 'white';
   canvas_context.stroke();
 }
 
+function render() {
+  new Computer;
+  new Player;
+  new Ball;
+}
+
 window.onload = function() {
-  Computer();
-  Player();
-  Ball();
+  render();
 }
